77fc92eac85e6b5f11a931d826c36a3c0fc269e5
Chirag Vindhani <cvindhani@codal.com>
2016-01-01 16:53:04 +0530

    Added all file

---
+++ gitDemo/gitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitDemo
+//
+//  Created by codalmacmini3 on 01/01/16.
+//  Copyright © 2016 codalmacmini3. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gitDemo/gitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitDemo
+//
+//  Created by codalmacmini3 on 01/01/16.
+//  Copyright © 2016 codalmacmini3. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
